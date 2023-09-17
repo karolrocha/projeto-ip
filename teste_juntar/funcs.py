cd5064bc2f5f2dd6ca37f1f7412f88439260cb82
Karolyne Rocha <karolrocha221@gmail.com>
2023-09-17 18:40:15 -0300

    adicionando plataformas e imagens

--- teste_juntar/funcs.py
+++ teste_juntar/funcs.py
@@ -24,13 +24,13 @@ JUMPING = 1
 FALLING = -1
         
 class Platform(Sprite):
-    def __init__(self,color):
+    def __init__(self, img, x, y):
         super().__init__()
-        self.image = pg.Surface((300,15), pg.SRCALPHA)
-        self.image.fill(color)
+        image = pg.image.load(img)
+        self.image = pg.transform.scale(image, (150, 15))
         self.rect = self.image.get_rect()
-        self.rect.x = 640
-        self.rect.y = 540
+        self.rect.x = x
+        self.rect.y = y
         self.group = Group()
         
     def update(self):
@@ -40,10 +40,10 @@ class Platform(Sprite):
         pass 
 
 class Moeda(Sprite):
-    def __init__(self, color, radius, screen_width, screen_height):
+    def __init__(self, img, radius, screen_width, screen_height):
         super().__init__()
-        self.image = pg.Surface((2 * radius, 2 * radius), pg.SRCALPHA)
-        pg.draw.circle(self.image, color, (radius, radius), radius)
+        image = pg.image.load(img)
+        self.image = pg.transform.scale(image, (30,30))
         self.rect = self.image.get_rect()
         self.rect.x = rd.randint(0, screen_width - 2 * radius)
         self.rect.y = rd.randint(0, screen_height - 2 * radius)
@@ -54,8 +54,8 @@ class Moeda(Sprite):
         pass
 
 def new_coins(moeda_group):  
-    if rd.randint(0, 100) < 0.01:  # Adicione uma moeda com uma probabilidade de 0.01%
-        moeda = Moeda((255, 255, 0), 10, DISPLAY_WIDTH, DISPLAY_HEIGHT)
+    if rd.randint(0, 1500) < 1:  # Adicione uma moeda com uma probabilidade de 5/1000
+        moeda = Moeda('images\moeda.png', 10, DISPLAY_WIDTH, DISPLAY_HEIGHT)
         moeda.rect.x = rd.randint(0, DISPLAY_WIDTH - moeda.rect.width)
         moeda.rect.y = rd.randint(0, DISPLAY_HEIGHT - moeda.rect.height)
         moeda_group.add(moeda)
--- teste_juntar/main_eventos.py
+++ teste_juntar/main_eventos.py
@@ -22,16 +22,20 @@ dt = 0 #segundos
 
 
 # dados do player
-player = Player('images/hero.png',(DISPLAY_WIDTH/2,0))
+player = Player('images/hero.png',(DISPLAY_WIDTH/2, DISPLAY_HEIGHT))
 height_p = player.rect.height
 width_p = player.rect.width
 
-plataforma = Platform((120,240,120))
+plataforma1 = Platform('images\plataforma.jpg', 150, 650 )
+plataforma2 = Platform('images\plataforma.jpg', 300, 500 )
+plataforma3 = Platform('images\plataforma.jpg', 450, 350 )
+plataforma4 = Platform('images\plataforma.jpg', 600, 200 )
+plataforma5 = Platform('images\plataforma.jpg', 750, 50 )
 plat_group = pg.sprite.Group()
-plat_group.add(plataforma)
+plat_group.add(plataforma1, plataforma2, plataforma3, plataforma4, plataforma5)
 
 moeda_group = pg.sprite.Group()  # Crie um grupo de sprites para as moedas
-moeda = Moeda((255, 255, 0), 10, DISPLAY_WIDTH, DISPLAY_HEIGHT)
+moeda = Moeda('images\moeda.png', 10, DISPLAY_WIDTH, DISPLAY_HEIGHT)
 
 all_sprites = pg.sprite.Group()
 
@@ -61,7 +65,7 @@ while running:
     
     # Display atualizado
     gamescreen.run()
-    score_text = font.render("SCORE: " + str(score), True, (255, 200, 100))
+    score_text = font.render("Moedas: " + str(score), True, (255, 200, 100))
     screen.blit(score_text, (50, 50))
 
     player.draw(screen)

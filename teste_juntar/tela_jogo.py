bf259a80834d4729e8dc26447c0c2512d741c87c
Karolyne Rocha <karolrocha221@gmail.com>
2023-09-25 15:58:08 -0300

    Organizando a Main

--- teste_juntar/tela_jogo.py
+++
@@ -1,9 +0,0 @@
-from funcs import *
-
-class Level():
-    def __init__(self,screen,background_img):
-        self.screen = screen
-        self.image = background_img
-
-    def run(self):
-        self.screen.blit(self.image,(0,0))
